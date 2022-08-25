db1fa1ec4f35fa4eb96c5365b13ca0407ea7f827
blockchainGuru1018 <as20190101@hotmail.com>
2022-08-25 20:59:59 +0800

    implement connection dict and and sending HTTP request

--- game/consumers.py
+++ game/consumers.py
@@ -1,5 +1,8 @@
 import json
+import requests
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+connection_list = {}
 
 
 class SocketAdapter(AsyncJsonWebsocketConsumer):
@@ -20,9 +23,14 @@ class SocketAdapter(AsyncJsonWebsocketConsumer):
         request = json.loads(text_data)
         method = request.get("method", None)
         params = request.get("params", None)
+        fields = {}
+        if 'fields' in params:
+            fields = params['fields']
         id = request.get("id", None)
 
         if method == 'setupTrigger':
+            path = fields["webhook_url"].strip("/").split("/")[-1]
+            connection_list[path] = self
             response = {
                 'jsonrpc': '2.0',
                 'result': {},
@@ -31,15 +39,54 @@ class SocketAdapter(AsyncJsonWebsocketConsumer):
             await self.send_json(response)
 
         if method == 'callWebhook':
-            run_action_response = {
+            path = fields["path"]
+            existed = next((i for i, d in enumerate(connection_list) if path in d), None)
+            if existed:
+                run_action_response = {
+                    'jsonrpc': '2.0',
+                    'result': {
+                        'key': 'notifySignal',
+                        'payload': params
+                    },
+                    'id': id
+                }
+                await connection_list[existed].send_json(run_action_response)
+            response = {
                 'jsonrpc': '2.0',
-                'result': {
-                    'key': 'notifySignal',
-                    'payload': params
-                },
+                'result': {},
                 'id': id
             }
-            await self.send_json(run_action_response)
+            await self.send_json(response)
+
+        if method == 'runAction':
+            request_method = ""
+            request_url = ""
+            request_data = ""
+            if "method" in fields:
+                request_method = fields['method']
+            if "url" in fields:
+                request_url = fields['url']
+            if "data" in fields:
+                request_data = json.loads(fields['data'])
+
+            if request_method == "GET":
+                r = requests.get(url=request_url, params=request_data)
+            if request_method == "POST":
+                r = requests.post(url=request_url, data=request_data)
+            if request_method == "PUT":
+                r = requests.put(url=request_url, data=request_data)
+            if request_method == "PATCH":
+                r = requests.patch(url=request_url, data=request_data)
+            if request_method == "DELETE":
+                r = requests.delete(url=request_url, data=request_data)
+            result = r.json()
+
+            response = {
+                'jsonrpc': '2.0',
+                'result': {},
+                'id': id
+            }
+            await self.send_json(response)
 
         if method == 'ping':
             response = {
